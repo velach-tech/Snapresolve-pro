ffb18e5b8667363daf1f05646723b7dca47c509f
velach-tech <victoryuche@icloud.com>
2025-11-03 08:50:54 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',  // <-- this makes it load correctly on GitHub Pages
+  base: '/Snapresolve-pro/', // 👈 your repository name, case-sensitive
 })
